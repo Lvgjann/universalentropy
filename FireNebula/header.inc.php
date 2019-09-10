062a1674d775b746d9cf922df67cfc01afef4ddd
florian.crampe <florian.crampe@etu.u-bordeaux.fr>
2019-09-10 10:22:57 +0200

    Updated files for next PHP migration (index section). The poem section will be updated later.

---
+++ FireNebula/header.inc.php
@@ -0,0 +1,8 @@
+<div id="menu">
+    <a href="../index.html">Universal Entropy</a>
+    <a href="index.html">Home</a>
+    <a href="poems.html">Poems</a>
+    <a href="novels.html">Novels</a>
+    <a href="drafts.html">Drafts</a>
+</div>
+<div id="banner"></div>
