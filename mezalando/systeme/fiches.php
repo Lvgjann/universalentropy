a4862492e760ffd5d40e0e38f907b7bfb4367eb4
NorthBlue333 <43409865+NorthBlue333@users.noreply.github.com>
2020-12-09 12:57:19 +0100

    fix: fix htaccess

--- mezalando/systeme/fiches.php
+++ mezalando/systeme/fiches.php
@@ -1,31 +1,6 @@
-<?php require '../../decide-lang.php';
-require_once('../../common.php');
-set_head(2, "mez", "Fiche"); ?>
-<body>
-
-<!-- Menu -->
-<?php set_menu('mez'); ?>
-
-<!-- Content -->
-<div id="page">
-    <div id="page_content">
-        <div class="title">
-            <span class="byline">Fiche de personnage</span></div>
-        <p>
-            <!--
-            <a href="template.pdf"> Download</a> -->
-            Refonte en cours.
-        </p>
-    </div>
-    <!-- Sidebar -->
-    <?php set_mezalando_sidebar(1); ?>
-</div>
-
-<!-- Portfolio -->
-<?php set_portfolio(2);
-
-// Copyright
-echo TXT_COPYRIGHT; ?>
-<script type="application/javascript" target="_parent" src="../../js/general_script.js"></script>
-</body>
-</html>
+<?php require('../../scripts/decide-lang.php');
+require_once('../../scripts/functions.php');
+ob_start();
+require('./fiches.html');
+$content = ob_get_clean();
+generate(2, "mez", "Fiche de personnage", $content); ?>
